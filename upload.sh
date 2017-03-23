878bfd06779d731bdf67edd6758d5a15191dc156
Shawn Betts <sabetts@andrew.cmu.edu>
2017-03-23 09:56:13 -0400

    add png files to upload script

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p staging
-cp -R sound *.js sf.html spinner.svg g.png thrust-illustration.svg  sf.css staging/
+cp -R sound *.js sf.html spinner.svg thrust-illustration.svg ccwise.png cwise.png g.png sf.css staging/
 rsync -aze "ssh -i mturk/mturk/shawn-laptop.pem" staging/ ec2-user@andersonlab.net:sfnavigation/www-staging
 rsync -aze "ssh -i mturk/mturk/shawn-laptop.pem" db/ ec2-user@andersonlab.net:sfnavigation/db
